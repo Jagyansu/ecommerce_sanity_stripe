eb946a6e677cdba3265223617047fc015e810e92
Jagyansu Padhy <jagyansup@example.com>
2023-07-13 20:16:31 +0530

    first commit

---
+++ sanity_ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '1yd48xy4',
+    dataset: 'production'
+  }
+})
---
+++ sanity_ecommerce/schemas/index.js
@@ -0,0 +1,5 @@
+import banner from "./banner";
+import product from "./product.js";
+
+
+export const schemaTypes = [product,banner]
